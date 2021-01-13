d94db802a263037b0fa6bb4ec43f2b59fa420a9c
Stephen Solis <ssolis8619@gmail.com>
2021-01-12 21:03:56 -0600

    created while statements to check user met requirements for password attributes.

--- Develop/script.js
+++ Develop/script.js
@@ -9,15 +9,25 @@ var numberS = "1234567890".split("");
 // Write password to the #password input
 function generatePassword() {
     var passLeng = prompt("Please select a length of characters between 8 - 128 characters");
+    
+    while(passLeng < 8 || passLeng > 128) {
+        alert("ALERT! Invalid number of characters, Please choose between 8 - 128");
+        passLeng = prompt("Please select a length of characters between 8 - 128 characters");
+    }
+    
     var uppCase = confirm("Do you want upper case letters?");
     var lwrCase = confirm("Do you want lower case characters?");
     var spcChar = confirm("Do you wnat special characters?");
     var numBrs = confirm("Do you want numbers included in your password?");
 
-    if(passLeng < 8 || passLeng > 128) {
-        alert("ALERT! Invalid number of characters, Please choose between 8 - 128");
-    }
-
+    while(uppCase == false && lwrCase == false && spcChar == false && numBrs == false) {
+        alert("INVALID! Please choose at least one character option.")
+        uppCase =   confirm("Do you want upper case letters?");
+        lwrCase =   confirm("Do you want lower case characters?");
+        spcChar =   confirm("Do you wnat special characters?");
+        numBrs =    confirm("Do you want numbers included in your password?");
+    }   
+    
     console.log(passLeng);
     console.log(uppCase);
     console.log(lwrCase);
