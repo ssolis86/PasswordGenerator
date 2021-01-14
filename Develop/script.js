2e9fbc0a378c313c2f1020d5a5c7cf584bcfb0be
Stephen Solis <ssolis8619@gmail.com>
2021-01-13 18:41:06 -0600

    created if statements to take in user's true statements on selections of password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var specialCharacters = "!@#$%^&*()-_+=".split("");
-var numberS = "1234567890".split("");
-
 
 // Write password to the #password input
 function generatePassword() {
+    var myString = "qwerty";
+    for (var i = 0; i < myString.length; i++) {
+        console.log(myString.charAt(i));
+    }
+    
     var passLeng = prompt("Please select a length of characters between 8 - 128 characters");
     
     while(passLeng < 8 || passLeng > 128) {
-        alert("ALERT! Invalid number of characters, Please choose between 8 - 128");
+        alert("ALERT! Invalid number of characters. Please choose between 8 - 128");
         passLeng = prompt("Please select a length of characters between 8 - 128 characters");
     }
     
     var uppCase = confirm("Do you want upper case letters?");
     var lwrCase = confirm("Do you want lower case characters?");
-    var spcChar = confirm("Do you wnat special characters?");
+    var spcChar = confirm("Do you want special characters?");
     var numBrs = confirm("Do you want numbers included in your password?");
 
     while(uppCase == false && lwrCase == false && spcChar == false && numBrs == false) {
@@ -28,14 +28,22 @@ function generatePassword() {
         numBrs =    confirm("Do you want numbers included in your password?");
     }   
     
-    console.log(passLeng);
-    console.log(uppCase);
-    console.log(lwrCase);
-    console.log(spcChar);
-    console.log(numBrs);
+    var charSet = "";
+    if(uppCase) {
+        charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } 
+    if(lwrCase) {
+        charSet += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if(spcChar) {
+        charSet += "!@#$%^&*()-_+="
+    }
+    if(numBrs) {
+        charSet += "1234567890"
+    }
+
+
 }
-
-
 
 function writePassword() {
   var password = generatePassword();
